b9f64809d93e0595b21818657b5ae0b014e7030e
lonecloud <lonecloudmac@icloud.com>
2017-12-22 23:31:26 +0800

    Initial Commit

---
+++ QQMessageInvoke/QQMessageInvoke.h
@@ -0,0 +1,13 @@
+//
+//  QQMessageInvoke.h
+//  QQMessageInvoke
+//
+//  Created by lonecloud on 2017/12/22.
+//  Copyright © 2017年 lonecloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QQMessageInvoke : NSObject
+
+@end
---
+++ QQMessageInvoke/QQMessageInvoke.m
@@ -0,0 +1,13 @@
+//
+//  QQMessageInvoke.m
+//  QQMessageInvoke
+//
+//  Created by lonecloud on 2017/12/22.
+//  Copyright © 2017年 lonecloud. All rights reserved.
+//
+
+#import "QQMessageInvoke.h"
+
+@implementation QQMessageInvoke
+
+@end
