306d9237e88717c4e8e6d66c4aecaf192302a154
lonecloud <lonecloudmac@icloud.com>
2018-03-16 14:02:18 +0800

    init project

--- QQMessageInvoke/QQMessageInvoke.m
+++ QQMessageInvoke/QQMessageInvoke.m
@@ -6,8 +6,20 @@
 //  Copyright © 2017年 lonecloud. All rights reserved.
 //
 
-#import "QQMessageInvoke.h"
+#import <Foundation/Foundation.h>
+#import "substrate/substrate.h"
 
-@implementation QQMessageInvoke
+void handleRecallNotifyIsOnline(id self, SEL _cmd, void * pVoid, BOOL isXXX)
+{
+    // nop
+    // 可以考虑加NSLog用于测试
+}
 
-@end
+__attribute__((constructor))
+static void initialize()
+{
+    // 使用@selector 会有编译警告，用NSSelectorFromString可消除。
+    // 我们的目标是：0 warning 0 error
+    SEL selector = NSSelectorFromString(@"handleRecallNotify:isOnline:");
+    MSHookMessageEx(NSClassFromString(@"QQMessageRevokeEngine"),selector,(IMP)&handleRecallNotifyIsOnline,NULL);
+}
