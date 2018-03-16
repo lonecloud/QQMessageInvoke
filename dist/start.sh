6690666cad88990d784fa5eb50932a13c1afee4c
lonecloud <lonecloudmac@icloud.com>
2018-03-16 14:08:48 +0800

    add dist file

---
+++ dist/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#判断当前位置
+echo 欢迎使用该防测回脚本
+qqpath=/Applications/QQ.app/Contents/MacOS
+echo $qqpath
+basepath=$(cd `dirname $0`; pwd);
+cd $basepath
+echo 处理的地址：$basepath
+cp libQQMessageInvoke.dylib $qqpath
+cp libsubstitute.dylib $qqpath
+echo 请在此处输入y
+#修改执行路径
+./insert_dylib $qqpath/libQQMessageInvoke.dylib $qqpath/QQ
+install_name_tool -change /usr/local/lib/libsubstitute.0.dylib @executable_path/libsubstitute.dylib $qqpath/libQQMessageInvoke.dylib
+install_name_tool -change $qqpath/libQQMessageInvoke.dylib @executable_path/libQQMessageInvoke.dylib $qqpath/QQ
+otool -L QQ | grep voke
+echo 正在启动qq
+mv $qqpath/QQ $qqpath/QQ_backup
+mv $qqpath/QQ_patched $qqpath/QQ
+$qqpath/QQ
+
