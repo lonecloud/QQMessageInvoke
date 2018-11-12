49d71decf5537d09a22fabf5982d631b5d5854e3
lonecloud <lonecloudmac@icloud.com>
2018-11-12 20:23:43 +0800

    add auto shell

---
+++ dist/install.sh
@@ -0,0 +1,16 @@
+main(){
+	GITHOME=~/QQMessageInvoke
+	echo '欢迎使用该QQ防撤回工具，请确认你的mac电脑安装了GIT'
+	if [[ -d $GITHOME ]]; then
+		rm -rf $GITHOME
+	fi
+	env git clone --depth=1 https://github.com/lonecloud/QQMessageInvoke.git "$GITHOME" || {
+    	printf "Error: 克隆QQ防撤回项目失败\n"
+    	exit 1
+  	}
+  	cd $GITHOME/dist
+  	expect auto.exp
+}
+
+
+main
